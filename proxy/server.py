c05a62974c0a5fc4b0d52d9bdff390811b05ed8c
acar12 <39702495+acar12@users.noreply.github.com>
2022-10-28 21:59:32 -0500

    (slightly) finished product

--- proxy/server.py
+++ proxy/server.py
@@ -3,26 +3,42 @@ import re
 
 pattern = re.compile(r"(?P<method>[a-zA-Z]+) (?P<uri>(\w+://)?(?P<host>[^\s\'\"<>\[\]{}|/:]+)(:(?P<port>\d+))?[^\s\'\"<>\[\]{}|]*) ")
 
+async def transfer_stream(reader, writer, close_event):
+    while not close_event.is_set():
+        try:
+            data = await asyncio.wait_for(reader.read(1024), 1)
+        except asyncio.TimeoutError:
+            continue
+        
+        if data == b"":
+            close_event.set()
+            print("Connection closed.")
+            break
+            
+        writer.write(data)
+        await writer.drain()
 
 async def handle_conn(reader, writer):
     client_request = (await reader.readuntil(b"\r\n\r\n")).decode()
-    print(client_request)
     
     if client_request.startswith("CONNECT"):
         request_regex = pattern.match(client_request)
 
         if request_regex:
             ws_reader, ws_writer = await asyncio.open_connection(request_regex.group("host"), request_regex.group("port"))
-            print(request_regex.group("host"), request_regex.group("port"))
+            print("Connecting to", request_regex.group("host"), ":", request_regex.group("port"))
+
+            writer.write(b"HTTP/1.1 200 Connection Established\r\n\r\n")
+            await writer.drain()
+
+            print("Connection made.")
+            close_event = asyncio.Event()
+            await asyncio.gather(
+                transfer_stream(reader, ws_writer, close_event),
+                transfer_stream(ws_reader, writer, close_event)
+            )
         else:
             writer.close()
-
-        writer.write(b"HTTP/1.1 200 Connection Established\r\n\r\n")
-        await writer.drain()
-        print("Connection made.")
-    
-        
-
 
 
 async def main():
